401b06b0bac7bac1997c12c0e073e2ca0d2fb613
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-02-23 13:07:10 +0530

    Update 1461. Check If a String Contains All Binary Codes of Size K.cpp

--- 1461. Check If a String Contains All Binary Codes of Size K.cpp
+++ 1461. Check If a String Contains All Binary Codes of Size K.cpp
@@ -24,3 +24,26 @@ Constraints:
 1 <= s.length <= 5 * 105
 s[i] is either '0' or '1'.
 1 <= k <= 20
+
+
+
+
+ //solution 
+ class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+       unordered_set<string> st;
+       int n = s.length();
+       int code = 1<<k;
+       for(int i = k; i <=n; i++){
+        string sub = s.substr(i-k, k);
+        if(!st.count(sub)){
+            st.insert(sub);
+            code--;
+        }if(code==0){
+            return true;
+        }
+       } 
+       return false;
+    }
+};
