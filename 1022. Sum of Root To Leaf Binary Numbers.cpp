da4385c2de2bf615179f6b205841940558b27065
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-02-24 11:40:42 +0530

    Update 1022. Sum of Root To Leaf Binary Numbers.cpp

--- 1022. Sum of Root To Leaf Binary Numbers.cpp
+++ 1022. Sum of Root To Leaf Binary Numbers.cpp
@@ -18,3 +18,14 @@ Constraints:
 
 The number of nodes in the tree is in the range [1, 1000].
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
