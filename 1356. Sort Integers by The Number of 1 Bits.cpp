ba23c1307b32aa628678ab000a26d0f7f7400869
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-02-25 11:20:42 +0530

    added solution.cpp

--- 1356. Sort Integers by The Number of 1 Bits.cpp
+++ 1356. Sort Integers by The Number of 1 Bits.cpp
@@ -29,3 +29,26 @@ Constraints:
 
 
   //solution
+class Solution {
+public:
+    int countBits(int n) {
+        int count = 0;
+        while (n) {
+            n = n & (n - 1);  
+            count++;
+        }
+        return count;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), [&](int a, int b) {
+            int bitsA = countBits(a);
+            int bitsB = countBits(b);
+
+            if (bitsA == bitsB)
+                return a < b;    
+            return bitsA < bitsB; 
+        });
+
+        return arr;
+    }
+};
