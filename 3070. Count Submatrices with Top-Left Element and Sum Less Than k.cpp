4ba74a4362168837709a9627a7e6d308a016895b
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-03-18 15:20:31 +0530

    problen

--- 3070. Count Submatrices with Top-Left Element and Sum Less Than k.cpp
+++ 3070. Count Submatrices with Top-Left Element and Sum Less Than k.cpp
@@ -1,1 +1,14 @@
 
+You are given a 0-indexed integer matrix grid and an integer k.
+
+Return the number of submatrices that contain the top-left element of the grid, and have a sum less than or equal to k.
+
+  Constraints:
+
+m == grid.length 
+n == grid[i].length
+1 <= n, m <= 1000 
+0 <= grid[i][j] <= 1000
+1 <= k <= 109
+
+  
