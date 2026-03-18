29799003b495005d162cb62dfd59a5de08e01928
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-03-18 16:31:39 +0530

    solution

--- 3070. Count Submatrices with Top-Left Element and Sum Less Than k.cpp
+++ 3070. Count Submatrices with Top-Left Element and Sum Less Than k.cpp
@@ -12,3 +12,23 @@ n == grid[i].length
 1 <= k <= 109
 
   
+//solution
+  class Solution {
+public:
+    int countSubmatrices(vector<vector<int>>& grid, int k) {
+        int n = grid.size(), m = grid[0].size();
+        vector<int> cols(m);
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            int rows = 0;
+            for (int j = 0; j < m; j++) {
+                cols[j] += grid[i][j];
+                rows += cols[j];
+                if (rows <= k) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+};
