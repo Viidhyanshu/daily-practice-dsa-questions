efdf239847528492c657df130e07f654ad4bf820
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-03-19 20:22:04 +0530

    Update 3212. Count Submatrices With Equal Frequency of X and Y.cpp

--- 3212. Count Submatrices With Equal Frequency of X and Y.cpp
+++ 3212. Count Submatrices With Equal Frequency of X and Y.cpp
@@ -1,1 +1,13 @@
+Given a 2D character matrix grid, where grid[i][j] is either 'X', 'Y', or '.', return the number of submatrices that contain:
+
+grid[0][0]
+an equal frequency of 'X' and 'Y'.
+at least one 'X'.
+
+Constraints:
+
+1 <= grid.length, grid[i].length <= 1000
+grid[i][j] is either 'X', 'Y', or '.'.
+
+//solution
 
