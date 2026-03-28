32ca12cb3e2fae07c9e1a352cfa03fcc94134371
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-03-28 16:21:17 +0530

    Create 2946. Matrix Similarity After Cyclic Shifts.cpp

---
+++ 2946. Matrix Similarity After Cyclic Shifts.cpp
@@ -0,0 +1,14 @@
+You are given an m x n integer matrix mat and an integer k. The matrix rows are 0-indexed.
+
+The following proccess happens k times:
+
+Even-indexed rows (0, 2, 4, ...) are cyclically shifted to the left.
+
+
+Odd-indexed rows (1, 3, 5, ...) are cyclically shifted to the right.
+
+
+Return true if the final modified matrix after k steps is identical to the original matrix, and false otherwise.
+
+
+  
