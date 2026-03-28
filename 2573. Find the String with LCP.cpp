b19002312898165010f94421a88306e57e947bd8
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-03-28 17:27:43 +0530

    Update 2573. Find the String with LCP.cpp

--- 2573. Find the String with LCP.cpp
+++ 2573. Find the String with LCP.cpp
@@ -87,3 +87,39 @@ public:
 //Greedy Allocation + Finding LCP and comparing
 //TC O(n^2)
 //SC O(n^2)
+
+
+
+
+//method 2
+class Solution {
+public:
+    static string findTheString(vector<vector<int>>& lcp) {
+        const int n = lcp.size();
+        string s(n, 'a' - 1);
+        int id = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] >= 'a')
+                continue;
+            if (++id > 26)
+                return "";
+            for (int j = 0; j < n; j++)
+                if (lcp[i][j])
+                    s[j] = 'a' + id - 1;
+        }
+        for (int i = 0; i < n; i++) {
+            if (lcp[i][i] != n - i)
+                return "";
+            for (int j = i + 1; j < n; j++) {
+                const int x = lcp[i][j];
+                if (x != lcp[j][i])
+                    return "";
+                int y = (i < n - 1 && j < n - 1) ? lcp[i + 1][j + 1] : 0;
+                y = (s[i] == s[j]) ? y + 1 : 0;
+                if (x != y)
+                    return "";
+            }
+        }
+        return s;
+    }
+};
