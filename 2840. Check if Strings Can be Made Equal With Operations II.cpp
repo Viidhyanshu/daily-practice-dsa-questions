d8ef03df40eb0f187e2fbdb701ce9011015d5661
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-03-31 21:23:53 +0530

    Add checkStrings function to compare two strings
    Implement checkStrings function to compare character counts in odd and even positions of two strings.

--- 2840. Check if Strings Can be Made Equal With Operations II.cpp
+++ 2840. Check if Strings Can be Made Equal With Operations II.cpp
@@ -29,4 +29,25 @@ s1 and s2 consist only of lowercase English letters.
 
 
   //solution
-  
+  class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        int n=s1.size();
+        map<char,int>st1,st2,st3,st4;
+        for(int i=0;i<n;i++){
+            if(i&1){
+                st1[s1[i]]++;
+                st3[s2[i]]++;
+            }
+            else{
+                st2[s1[i]]++;
+                st4[s2[i]]++;
+            }
+        }
+        if(st1==st3 && st2==st4){
+            return true;
+        }
+        return false;
+    }
+};
+
