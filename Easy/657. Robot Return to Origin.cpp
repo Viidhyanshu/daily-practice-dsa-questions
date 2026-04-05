30f22f391c8b5cf71f1af5e696675f12adb7c04d
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-04-05 16:07:49 +0530

    Update 657. Robot Return to Origin.cpp

--- Easy/657. Robot Return to Origin.cpp
+++ Easy/657. Robot Return to Origin.cpp
@@ -29,3 +29,17 @@ moves only contains the characters 'U', 'D', 'L' and 'R'.
 
   //solution
   
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x =0;
+        int y =0;
+        for(char &ch : moves){
+            if(ch == 'U') y++;
+            else if(ch =='D') y--;
+            else if(ch =='L') x--;
+            else if(ch =='R') x++;
+        }
+        return x==0 && y==0;
+    }
+};
