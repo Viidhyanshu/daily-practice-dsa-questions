d34c8a5d0510eca5ca1efa841aea73c7fb0cbe9b
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-04-10 16:42:29 +0530

    Update 3740. Minimum Distance Between Three Equal Elements I.cpp

--- Easy/3740. Minimum Distance Between Three Equal Elements I.cpp
+++ Easy/3740. Minimum Distance Between Three Equal Elements I.cpp
@@ -52,3 +52,29 @@ Constraints:
 
   //solution
   
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int n = nums.size();
+        int res = n + 1;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                if (nums[i] != nums[j]) {
+                    continue;
+                }
+
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[j] == nums[k]) {
+                        res = min(res, k - i);
+                        break;
+                    }
+                }
+            }
+        }
+        if (res == n + 1) {
+            return -1;
+        } else {
+            return res * 2;
+        }
+    }
+};
