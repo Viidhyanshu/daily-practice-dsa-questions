81fde6605753ffed99bb654e43f372366afc6c20
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-04-12 20:59:13 +0530

    Update 1320. Minimum Distance to Type a Word Using Two Fingers.cpp

--- Hard/1320. Minimum Distance to Type a Word Using Two Fingers.cpp
+++ Hard/1320. Minimum Distance to Type a Word Using Two Fingers.cpp
@@ -40,4 +40,44 @@ word consists of uppercase English letters.
 
   //solution
 
-  
+ Approach- Recursion and Memo with 5-D Memoization DP
+
+class Solution {
+public:
+    int dp[301][7][7][7][7];
+    pair<int, int> getCoordinate(char ch) {
+        int pos = ch - 'A';
+        return {pos / 6, pos % 6};
+    }
+    int getDistance(int x1, int y1, int x2, int y2) {
+        return abs(x1 - x2) + abs(y1 - y2);
+    }
+    int solve(string &word, int i, int x1, int y1, int x2, int y2) {
+        if (i == word.size()) 
+            return 0;
+        if (dp[i][x1 + 1][y1 + 1][x2 + 1][y2 + 1] != -1)
+            return dp[i][x1 + 1][y1 + 1][x2 + 1][y2 + 1];
+        auto [x, y] = getCoordinate(word[i]);
+        if (x1 == -1 && y1 == -1 && x2 == -1 && y2 == -1) {         //first finger not used
+            return dp[i][x1 + 1][y1 + 1][x2 + 1][y2 + 1] = solve(word, i + 1, x, y, x2, y2); 
+        }
+        if (x2 == -1 && y2 == -1) {                  //second finger not used
+            int moveF2 = solve(word, i + 1, x1, y1, x, y);
+
+            int moveF1 = solve(word, i + 1, x, y, x2, y2) 
+                             + getDistance(x1, y1, x, y);
+            return dp[i][x1 + 1][y1 + 1][x2 + 1][y2 + 1] = min(moveF1, moveF2);
+        }
+        int moveF1 = solve(word, i + 1, x, y, x2, y2)       //both fingers used
+                     + getDistance(x1, y1, x, y);
+        int moveF2 = solve(word, i + 1, x1, y1, x, y) 
+                     + getDistance(x2, y2, x, y);
+        return dp[i][x1 + 1][y1 + 1][x2 + 1][y2 + 1] = min(moveF1, moveF2);
+    }
+    int minimumDistance(string word) {
+        memset(dp, -1, sizeof(dp));
+        return solve(word, 0, -1, -1, -1, -1);
+    }
+};
+T.C. = O(n*27*27) ~= O(n)
+S.C. = O(n*27*27) ~= O(n)
