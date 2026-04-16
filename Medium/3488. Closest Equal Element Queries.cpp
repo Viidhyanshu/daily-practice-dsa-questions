a1a35c8591b97702af0e4824231079dc15b93015
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-04-16 16:19:40 +0530

    Update 3488. Closest Equal Element Queries.cpp

--- Medium/3488. Closest Equal Element Queries.cpp
+++ Medium/3488. Closest Equal Element Queries.cpp
@@ -38,4 +38,41 @@ Constraints:
 
 
   //solution
-  
+  class Solution {
+public:
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size();
+
+        unordered_map<int, vector<int>> mp;
+        for(int i = 0 ; i < n; i++){
+            mp[nums[i]].push_back(i);
+        }
+        vector <int> result;
+        for(int qi : queries){
+            int element = nums[qi];
+                vector<int> & vec = mp[element];
+                int sz = vec.size();
+                if(sz == 1){
+                    result.push_back(-1);
+                    continue;
+                }
+            int pos = lower_bound(begin(vec), end(vec), qi) - begin(vec);
+            int res = INT_MAX;
+            int right = vec[(pos + 1)%sz];
+            int dist = abs(right - qi);
+            int circDist = n - dist;  
+            res = min({res, dist, circDist});
+
+            int left = vec[(pos - 1 + sz)%sz];
+            dist = abs(left - qi);
+            circDist = n - dist;  
+            res = min({res, dist, circDist});
+
+            result.push_back(res);
+        }
+        return result;
+    }
+};
+
+T.C : O(nlogn)
+S.C : O(n)
