d166cc1ca78cd0d8aff5f0dd96c091e981ef8571
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-04-19 12:43:40 +0530

    Update 1855. Maximum Distance Between a Pair of Values.cpp

--- Medium/1855. Maximum Distance Between a Pair of Values.cpp
+++ Medium/1855. Maximum Distance Between a Pair of Values.cpp
@@ -35,4 +35,25 @@ Constraints:
 Both nums1 and nums2 are non-increasing.
 
   //solution
-  
+  class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        int i =0;
+        int j =0;
+        int result=0;
+        while(i < n && j < m){
+            if(nums1[i] > nums2[j]){
+                i++;
+            }else{
+                result = max(result, j-i);
+                j++;
+            }
+        }
+        return result;
+    }
+};
+
+TC = O(n+m)
+ SC = Constant
