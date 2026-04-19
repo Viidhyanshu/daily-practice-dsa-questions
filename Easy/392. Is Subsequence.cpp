8c7d6647dc858a5debe76999a095a349fd3ad922
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-04-19 12:52:42 +0530

    Update 392. Is Subsequence.cpp

--- Easy/392. Is Subsequence.cpp
+++ Easy/392. Is Subsequence.cpp
@@ -26,3 +26,20 @@ Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 
 
   //solution
   
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i =0;
+        int j =0;
+        while (i < s.size() && j < t.size()){
+            if(s[i]==t[j]){
+                i++;
+            }
+            j++;
+        }
+        return i == s.size();
+    }
+};
+
+TC = O(N)
+ SC = constant;
