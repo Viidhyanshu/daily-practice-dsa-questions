c99b14c092103ffe9393406b7f8a3c0df4a640d0
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-04-21 13:17:56 +0530

    Update 2078. Two Furthest Houses With Different Colors.cpp

--- Easy/2078. Two Furthest Houses With Different Colors.cpp
+++ Easy/2078. Two Furthest Houses With Different Colors.cpp
@@ -6,3 +6,19 @@ The distance between the ith and jth houses is abs(i - j), where abs(x) is the a
 
 
 //solution
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n = colors.size();
+        int result = 0;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (colors[i] != colors[j]) {
+                    result = max(result, j - i);
+                }
+            }
+        }
+        return result;
+    }
+};
