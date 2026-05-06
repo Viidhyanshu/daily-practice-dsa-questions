1e7f71842ba7edbc0fc5ae1ecc39f18794559474
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-05-06 17:03:17 +0530

    Update 1861. Rotating the Box.cpp

--- Medium/1861. Rotating the Box.cpp
+++ Medium/1861. Rotating the Box.cpp
@@ -8,3 +8,46 @@ The box is rotated 90 degrees clockwise, causing some of the stones to fall due 
 It is guaranteed that each stone in boxGrid rests on an obstacle, another stone, or the bottom of the box.
 
 Return an n x m matrix representing the box after the rotation described above.
+
+  //solution
+  class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m = box.size();
+        int n = box[0].size();
+
+        vector<vector<char>> result(n, vector<char>(m));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                result[i][j] = box[j][i];
+            }
+        }
+        for(vector<char>& row : result) {
+            reverse(begin(row), end(row));
+        }
+        for(int j = 0; j < m; j++) {
+            for(int i = n-1; i >= 0; i--) {
+                if(result[i][j] == '.') { 
+                    int stoneRow = -1;
+
+                    for(int k = i-1; k >= 0; k--) {
+                        if(result[k][j] == '*') {
+                            break;
+                        } else if(result[k][j] == '#') {
+                            stoneRow = k;
+                            break;
+                        }
+                    }
+
+                    if(stoneRow != -1) {
+                        result[i][j] = '#';
+                        result[stoneRow][j] = '.';
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
