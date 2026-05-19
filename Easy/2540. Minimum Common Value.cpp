c0d30cfc4d5c397434571a5e7deb3f2649dd010b
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-05-19 19:22:59 +0530

    Update 2540. Minimum Common Value.cpp

--- Easy/2540. Minimum Common Value.cpp
+++ Easy/2540. Minimum Common Value.cpp
@@ -24,3 +24,27 @@ Both nums1 and nums2 are sorted in non-decreasing order.
  
 
 //solution
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        int i = 0; 
+        int j = 0; 
+        
+        while(i < m && j < n) {
+            if(nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else if(nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        return -1;
+    }
+};
+
+T.C. = O(m*n)
+S.C. = O(1) 
