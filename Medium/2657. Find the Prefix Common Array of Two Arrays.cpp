94b8af0a72b9c29b5432b671f62cc8e849aa1969
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-05-20 21:14:59 +0530

    Update 2657. Find the Prefix Common Array of Two Arrays.cpp

--- Medium/2657. Find the Prefix Common Array of Two Arrays.cpp
+++ Medium/2657. Find the Prefix Common Array of Two Arrays.cpp
@@ -31,4 +31,29 @@ Constraints:
 1 <= A[i], B[i] <= n
 It is guaranteed that A and B are both a permutation of n integers.
 //solution
-  
+  //method 1
+ T.C. = O(n^3)
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        vector<int> result(n);
+        for(int i = 0 ; i < n; i++) {  
+          int count = 0;
+
+          for(int x = 0; x <= i; x++) {
+                for(int y = 0; y <= i; y++) { 
+                    if(B[y] == A[x]) {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            result[i] = count;
+        }
+        return result;
+    }
+};
+
+
+//method 2
