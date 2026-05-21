090c25d28e08963dc912e11c0e4975824f3d3d7c
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-05-21 20:58:20 +0530

    Update 3043. Find the Length of the Longest Common Prefix.cpp

--- Medium/3043. Find the Length of the Longest Common Prefix.cpp
+++ Medium/3043. Find the Length of the Longest Common Prefix.cpp
@@ -31,3 +31,32 @@ Constraints:
 
 1 <= arr1.length, arr2.length <= 5 * 104
 1 <= arr1[i], arr2[i] <= 108
+
+
+
+ //solution
+ class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        unordered_set<int> st;
+        for(int val : arr1) {
+            while(!st.count(val) && val > 0) {
+                st.insert(val);
+
+                val = val/10;
+            }
+        }
+        int result = 0; 
+        for(int num : arr2) {
+            while(!st.count(num) && num > 0) {
+                num /= 10;
+            }
+
+            if(num > 0) {
+                result = max(result, static_cast<int>(log10(num)) + 1);
+            }
+        }
+
+        return result;
+    }
+};
