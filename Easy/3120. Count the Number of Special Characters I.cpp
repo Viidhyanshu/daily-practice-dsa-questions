0105223405492b9b75bb06eea294b35310bf878c
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-05-26 16:09:46 +0530

    Update 3120. Count the Number of Special Characters I.cpp

--- Easy/3120. Count the Number of Special Characters I.cpp
+++ Easy/3120. Count the Number of Special Characters I.cpp
@@ -45,3 +45,25 @@ word consists of only lowercase and uppercase English letters.
 
   //solution
   
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        vector<bool> low(26, false);
+        vector<bool> up(26, false);
+        for (char c : word) {
+            if (c >= 'a' && c <= 'z') {
+                low[c - 'a'] = true;
+            }
+            else {
+                up[c - 'A'] = true;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if (low[i] && up[i]) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
