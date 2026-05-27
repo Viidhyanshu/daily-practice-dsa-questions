0a5d30edf0b40b11886da4a604492d82a1d932dd
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-05-27 18:43:46 +0530

    Update 3121. Count the Number of Special Characters II.cpp

--- Medium/3121. Count the Number of Special Characters II.cpp
+++ Medium/3121. Count the Number of Special Characters II.cpp
@@ -44,4 +44,40 @@ word consists of only lowercase and uppercase English letters.
 
 
   //solution
-  
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        
+        vector<int> lastSmall(26, -1);
+        vector<int> firstCapital(26, -1);
+
+        for(int i = 0; i < word.length(); i++) {
+            
+            char ch = word[i];
+
+            if(islower(ch)) {
+                lastSmall[ch - 'a'] = i;
+            }
+            else {
+                if(firstCapital[ch - 'A'] == -1) {
+                    firstCapital[ch - 'A'] = i;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for(int i = 0; i < 26; i++) {
+
+            if(lastSmall[i] != -1 && firstCapital[i] != -1 &&
+               lastSmall[i] < firstCapital[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+ Approach = find last occurrence of lowercase and first occurrence of uppercase
+T.C = O(n)
