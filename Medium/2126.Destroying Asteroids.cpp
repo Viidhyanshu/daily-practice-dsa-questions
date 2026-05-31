e46e06ab527630f44a4134f49513792af17aeecb
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-05-31 19:02:18 +0530

    Update 2126.Destroying Asteroids.cpp

--- Medium/2126.Destroying Asteroids.cpp
+++ Medium/2126.Destroying Asteroids.cpp
@@ -1,1 +1,18 @@
 
+//solution
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(begin(asteroids), end(asteroids));
+        for(int &asteroid : asteroids){
+            if(mass < asteroid)
+            return false;
+            else if(mass >= 1e5)
+            return true;
+            mass += asteroid;
+        }
+        return true;
+    }
+};
+T.C. = O(nlogN)
+  
