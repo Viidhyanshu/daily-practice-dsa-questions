a4926b5b7a1d2d0c65cf0f7bd9b92455ad78858c
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-06-02 17:04:08 +0530

    Update 3633. Earliest Finish Time for Land and Water Rides I.cpp

--- Easy/3633. Earliest Finish Time for Land and Water Rides I.cpp
+++ Easy/3633. Earliest Finish Time for Land and Water Rides I.cpp
@@ -65,4 +65,18 @@ waterStartTime.length == waterDuration.length == m
 
 
 //solution
-  
+  class Solution {
+public:
+    int earliestFinishTime(vector<int>& ls, vector<int>& ld, vector<int>& ws, vector<int>& wd) {
+        int ans = INT_MAX;
+
+        for (int i = 0; i < ls.size(); i++)
+            for (int j = 0; j < ws.size(); j++) {
+                ans = min(ans, max(ls[i] + ld[i], ws[j]) + wd[j]);
+                ans = min(ans, max(ws[j] + wd[j], ls[i]) + ld[i]);
+            }
+
+        return ans;
+    }
+};
+T.C. = O(N*M)
