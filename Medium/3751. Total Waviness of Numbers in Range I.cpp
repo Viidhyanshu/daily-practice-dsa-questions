7018cf6f751890337131625aa538296a960a8d0a
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-06-04 15:04:31 +0530

    Update 3751. Total Waviness of Numbers in Range I.cpp

--- Medium/3751. Total Waviness of Numbers in Range I.cpp
+++ Medium/3751. Total Waviness of Numbers in Range I.cpp
@@ -54,3 +54,33 @@ Number 4848: the second digit 8 is a peak, and the third digit 4 is a valley, gi
 Constraints:
 
 1 <= num1 <= num2 <= 105
+
+
+
+ //solution
+                
+class Solution {
+public:
+    int findWaiveScore(int num) {
+        string s = to_string(num);
+        int l = s.length();
+        if (l < 3)
+            return 0;
+        int score = 0;
+        for (int i = 1; i <= l - 2; i++) {
+            if (s[i] > s[i - 1] && s[i] > s[i + 1]) score++;
+            if (s[i] < s[i - 1] && s[i] < s[i + 1]) score++;
+        }
+        return score;
+    }
+    int totalWaviness(int num1, int num2) {
+        int score = 0;
+        for (int num = num1; num <= num2; num++) {
+            score += findWaiveScore(num);
+        }
+        return score;
+    }
+};
+
+T.C = O((num2 - num1)*log10(num2)) 
+S.C =O(log10(num2))
