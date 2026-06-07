f0115116dbb90f899b46f5b5040e1e6ce8841fd5
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-06-07 11:29:11 +0530

    Update 2196. Create Binary Tree From Descriptions.cpp

--- Medium/2196. Create Binary Tree From Descriptions.cpp
+++ Medium/2196. Create Binary Tree From Descriptions.cpp
@@ -1,1 +1,19 @@
+You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+
+If isLefti == 1, then childi is the left child of parenti.
+If isLefti == 0, then childi is the right child of parenti.
+Construct the binary tree described by descriptions and return its root.
+
+The test cases will be generated such that the binary tree is valid.
+
+Constraints:
+
+1 <= descriptions.length <= 104
+descriptions[i].length == 3
+1 <= parenti, childi <= 105
+0 <= isLefti <= 1
+The binary tree described by descriptions is valid.
+
+
+//solution
 
