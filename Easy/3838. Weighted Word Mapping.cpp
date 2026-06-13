9ec56bcc3f79e3aab56cd1a086e9eab021973e05
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-06-13 17:49:51 +0530

    Update 3838. Weighted Word Mapping.cpp

--- Easy/3838. Weighted Word Mapping.cpp
+++ Easy/3838. Weighted Word Mapping.cpp
@@ -59,3 +59,18 @@ words[i] consists of lowercase English letters.
 
 
   //solution
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+        ans.reserve(words.size());
+        for (string word : words) {
+            int s = 0;
+            for (char c : word) {
+                s += weights[c - 'a'];
+            }
+            ans += 'z' - s % 26;
+        }
+        return ans;
+    }
+};
