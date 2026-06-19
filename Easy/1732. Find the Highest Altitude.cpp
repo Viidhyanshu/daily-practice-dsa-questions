cb508a460ea1d244be83c9018dc6fac264613b4d
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-06-19 18:25:15 +0530

    Update 1732. Find the Highest Altitude.cpp

--- Easy/1732. Find the Highest Altitude.cpp
+++ Easy/1732. Find the Highest Altitude.cpp
@@ -21,3 +21,17 @@ Constraints:
 n == gain.length
 1 <= n <= 100
 -100 <= gain[i] <= 100
+
+//solution
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int curr = 0;
+        int highest = curr;
+        for (int gain : gain) {
+            curr += gain;
+            highest = max(highest, curr);
+        }
+        return highest;
+    }
+};
