410da07366165bdc4ce4fbf9f420f5a6fc9e80a8
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-06-24 11:00:09 +0530

    Update 1189. Maximum Number of Balloons.cpp

--- Easy/1189. Maximum Number of Balloons.cpp
+++ Easy/1189. Maximum Number of Balloons.cpp
@@ -26,3 +26,24 @@ Constraints:
 
 1 <= text.length <= 104
 text consists of lower case English letters only.
+
+
+ //solution
+ class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        vector<int> freq(26, 0);
+
+        for (char c : text) {
+            freq[c - 'a']++;
+        }
+
+        return min({
+            freq['b' - 'a'],
+            freq['a' - 'a'],
+            freq['l' - 'a'] / 2,
+            freq['o' - 'a'] / 2,
+            freq['n' - 'a']
+        });
+    }
+};
