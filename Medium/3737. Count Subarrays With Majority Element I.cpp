7a989e61f7b75e22ed7e249a7c9a86939bbdfecd
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-06-25 19:22:35 +0530

    Update 3737. Count Subarrays With Majority Element I.cpp

--- Medium/3737. Count Subarrays With Majority Element I.cpp
+++ Medium/3737. Count Subarrays With Majority Element I.cpp
@@ -52,4 +52,32 @@ Constraints:
 1 <= target <= 109
 
 //solution
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == target)
+                nums[i] = 1;
+            else
+                nums[i] = -1;
+        }
+        for(int i = 1; i < n; i++) {
+            nums[i] += nums[i-1];
+        }
 
+        long long ans = 0;
+        for (int l = 0; l < n; l++) {
+            for (int r = l; r < n; r++) {
+                int sum = nums[r] - ((l > 0) ? nums[l-1] : 0);
+
+                if(sum > 0)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+
+t.c. = o(n^2)
