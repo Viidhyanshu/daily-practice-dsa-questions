7081ca386e5f47695278a2b57be74384e5d79648
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-06-26 20:22:35 +0530

    Update 3739. Count Subarrays With Majority Element II.cpp

--- Hard/3739. Count Subarrays With Majority Element II.cpp
+++ Hard/3739. Count Subarrays With Majority Element II.cpp
@@ -50,3 +50,29 @@ Constraints:
 1 <= nums.length <= 10​​​​​​​5
 1 <= nums[i] <= 10​​​​​​​9
 1 <= target <= 109
+
+//solution
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        int currSum = 0;
+        mp[0] = 1;
+        long long validLeftPoints = 0;
+        long long result = 0;
+        for (int x : nums) {
+            if (x == target) {
+                validLeftPoints += mp[currSum];
+                currSum++;
+            } else {
+                currSum--;
+                validLeftPoints -= mp[currSum];
+            }
+            mp[currSum]++;
+            result += validLeftPoints;
+        }
+        return result;
+    }
+};
+
+t.c. = o(n)
