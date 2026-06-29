ee7990da3b3d29c62e04dfd4fbf81cf6b79361c2
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-06-29 20:21:06 +0530

    Update 1967. Number of Strings That Appear as Substrings in Word.cpp

--- Easy/1967. Number of Strings That Appear as Substrings in Word.cpp
+++ Easy/1967. Number of Strings That Appear as Substrings in Word.cpp
@@ -39,4 +39,34 @@ patterns[i] and word consist of lowercase English letters.
 
 
   //solution
-  
+  class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        auto check = [](const string& pattern, const string& word) -> bool {
+            int m = pattern.size();
+            int n = word.size();
+            for (int i = 0; i + m <= n; ++i) {
+                bool flag = true;
+                for (int j = 0; j < m; ++j) {
+                    if (word[i + j] != pattern[j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        int res = 0;
+        for (const string& pattern : patterns) {
+            res += check(pattern, word);
+        }
+        return res;
+    }
+};
+
+
+t.c. = o(m*n*k)
