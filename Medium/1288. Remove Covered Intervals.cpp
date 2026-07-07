c6c7e73006eb4178c0179e60d4a69f8ed26f044e
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-07-07 16:33:36 +0530

    Update 1288. Remove Covered Intervals.cpp

--- Medium/1288. Remove Covered Intervals.cpp
+++ Medium/1288. Remove Covered Intervals.cpp
@@ -23,3 +23,31 @@ Constraints:
 intervals[i].length == 2
 0 <= li < ri <= 105
 All the given intervals are unique.
+
+
+ //solution
+ class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        vector<vector<int>> result;
+        int n = intervals.size();
+        auto lambda = [](vector<int>& vec1, vector<int>& vec2) {
+            if(vec1[0] == vec2[0]) {
+                return vec1[1] > vec2[1];
+            }
+            return vec1[0] < vec2[0];
+        };
+        sort(begin(intervals), end(intervals), lambda);
+        result.push_back(intervals[0]);
+        for(int i = 1; i < n; i++) {
+            if(result.back()[0] <= intervals[i][0] && result.back()[1] >= intervals[i][1]) {
+                continue;
+            }
+            result.push_back(intervals[i]);
+        }
+        return result.size();
+    }
+};
+
+
+t.c. = o(nlogn)
