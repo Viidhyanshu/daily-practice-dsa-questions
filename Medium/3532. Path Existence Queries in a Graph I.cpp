b9a1d6376acb6414295fb185137f176b378a0949
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-07-09 19:31:01 +0530

    Update 3532. Path Existence Queries in a Graph I.cpp

--- Medium/3532. Path Existence Queries in a Graph I.cpp
+++ Medium/3532. Path Existence Queries in a Graph I.cpp
@@ -49,3 +49,35 @@ nums is sorted in non-decreasing order.
 1 <= queries.length <= 105
 queries[i] == [ui, vi]
 0 <= ui, vi < n
+
+
+ //solution
+ class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<int> comp(n);
+
+        for (int i = 1; i < n; i++) {
+            int diff = abs(nums[i] - nums[i - 1]);
+            if (diff <= maxDiff)
+                comp[i] = comp[i - 1];
+            else
+                comp[i] = i;
+        }
+
+        vector<bool> ans(queries.size(), false);
+
+        for (int i = 0; i < queries.size(); i++) {
+            int node1 = queries[i][0];
+            int node2 = queries[i][1];
+
+            if (comp[node1] == comp[node2])
+                ans[i] = true;
+        }
+
+        return ans;
+    }
+};
+
+t.c.  = o (n + m)
+s.c. = o(n)
