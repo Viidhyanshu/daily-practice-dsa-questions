005494df8dd72c1100b1bc7d19d6d1f393ea4c85
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-07-13 15:11:51 +0530

    Update 1291. Sequential Digits.cpp

--- Medium/1291. Sequential Digits.cpp
+++ Medium/1291. Sequential Digits.cpp
@@ -17,3 +17,33 @@ Output: [1234,2345,3456,4567,5678,6789,12345]
 Constraints:
 
 10 <= low <= high <= 10^9
+
+
+ //solution
+ class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        queue<int> que;
+        for(int i = 1; i <= 8; i++) {
+            que.push(i);
+        }
+        
+        vector<int> result;
+        
+        while(!que.empty()) {
+            int temp = que.front();
+            que.pop();
+            
+            if(temp >= low && temp <= high) {
+                result.push_back(temp);
+            }
+            
+            int last_digit = temp%10;
+            if(last_digit + 1 <= 9) {
+                que.push(temp * 10 +(last_digit + 1));
+            }
+        }
+        
+        return result;
+    }
+};
