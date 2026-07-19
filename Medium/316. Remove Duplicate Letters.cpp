ca83d27cedbb0e3f74a376d381970e0760c919a0
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-07-19 20:07:27 +0530

    Update 316. Remove Duplicate Letters.cpp

--- Medium/316. Remove Duplicate Letters.cpp
+++ Medium/316. Remove Duplicate Letters.cpp
@@ -19,3 +19,32 @@ s consists of lowercase English letters.
 
   //solution
   
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n = s.length();
+        string result;
+        vector<bool> taken(26, false);
+        vector<int> lastIdx(26);
+
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            lastIdx[ch - 'a'] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            int idx = s[i] - 'a';
+            if (taken[idx] == true)
+                continue;
+            while (result.length() > 0 && s[i] < result.back() &&
+                   lastIdx[result.back() - 'a'] > i) {
+                taken[result.back() - 'a'] = false;
+                result.pop_back();
+            }
+            result.push_back(s[i]);
+            taken[idx] = true;
+        }
+        return result;
+    }
+};
+
+t.c. = o(n)
