6b8aa99736c29fed0346be32eb929b009a50c992
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-07-20 10:17:57 +0530

    Update 1260. Shift 2D Grid.cpp

--- Easy/1260. Shift 2D Grid.cpp
+++ Easy/1260. Shift 2D Grid.cpp
@@ -16,3 +16,25 @@ n == grid[i].length
 0 <= k <= 100
 
 //solution
+
+class Solution {
+  public:
+  vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k){
+    int n = grid.size();
+    int m = grid[0].size();
+    while(k--){
+      for(int i = 0; i < n; i++){
+        for(int j = m-1; j > 0; j--){
+          swap(grid[i][j], grid[i][j-1]);
+        }
+      }
+      for(int i = n-1; i > 0; i++){
+          swap(grid[i][0], grid[i-1][0]);
+      }
+    }
+    return grid;
+  }
+};
+
+
+t.c. = o(k*m*n)
