ad17d45327ff9e729c4bc59d9d09b64313c9b8d7
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-07-23 11:06:53 +0530

    Update 3513. Number of Unique XOR Triplets I.cpp

--- Medium/3513. Number of Unique XOR Triplets I.cpp
+++ Medium/3513. Number of Unique XOR Triplets I.cpp
@@ -48,3 +48,17 @@ nums is a permutation of integers from 1 to n.
 
   //solution
   
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+       int n = nums.size();
+       if(n <=2 )
+       return n;
+       int result = 1;
+       while(result <= n)
+       result <<= 1;
+       return result;
+    }
+};
+
+t.c. = o(logN)
