39808207312f54facff1280b31e3cdb11a01bca7
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-07-28 21:03:42 +0530

    Update 3517. Smallest Palindromic Rearrangement I.cpp

--- Medium/3517. Smallest Palindromic Rearrangement I.cpp
+++ Medium/3517. Smallest Palindromic Rearrangement I.cpp
@@ -45,3 +45,20 @@ s is guaranteed to be palindromic.
 
   //solution
   
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n   = s.length();
+        int mid = n/2;
+
+        sort(s.begin(), s.begin() + mid); 
+
+        for (int i = 0; i < mid; i++) {
+            s[n - 1 - i] = s[i];
+        }
+
+        return s;
+    }
+};
+
+t.c. = O(nlogn)
