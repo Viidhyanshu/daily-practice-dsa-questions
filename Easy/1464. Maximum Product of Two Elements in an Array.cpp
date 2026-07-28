7859127c29ac46a20c9b9f01b3893619cee450f0
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-07-28 21:05:14 +0530

    Add maxProduct function for maximum product calculation
    Implement maxProduct function to calculate maximum product of two elements in an array.

--- Easy/1464. Maximum Product of Two Elements in an Array.cpp
+++ Easy/1464. Maximum Product of Two Elements in an Array.cpp
@@ -21,3 +21,19 @@ Constraints:
 
 2 <= nums.length <= 500
 1 <= nums[i] <= 10^3
+
+
+ //solution
+ class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                ans = max(ans, (nums[i] - 1) * (nums[j] - 1));
+            }
+        }
+        
+        return ans;
+    }
+};
