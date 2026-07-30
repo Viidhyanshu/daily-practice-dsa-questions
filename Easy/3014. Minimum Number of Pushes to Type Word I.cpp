859b0e1cbc11f9db812c5e36229cdba3b917b1a5
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-07-30 23:01:53 +0530

    Update 3014. Minimum Number of Pushes to Type Word I.cpp

--- Easy/3014. Minimum Number of Pushes to Type Word I.cpp
+++ Easy/3014. Minimum Number of Pushes to Type Word I.cpp
@@ -16,3 +16,14 @@ word consists of lowercase English letters.
 All letters in word are distinct.
 
   //solution
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            res += i / 8 + 1;
+        }
+        return res;
+    }
+};
