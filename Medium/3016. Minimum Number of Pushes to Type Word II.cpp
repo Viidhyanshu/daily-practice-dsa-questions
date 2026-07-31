6938f0c0fb5eb117cd0ea7a788a936a27bb1ecfa
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-07-31 17:20:10 +0530

    Update 3016. Minimum Number of Pushes to Type Word II.cpp

--- Medium/3016. Minimum Number of Pushes to Type Word II.cpp
+++ Medium/3016. Minimum Number of Pushes to Type Word II.cpp
@@ -29,3 +29,24 @@ Constraints:
 
 1 <= word.length <= 105
 word consists of lowercase English letters.
+
+
+ //solution
+ class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> mp(26, 0);
+        for(char &ch: word)
+            mp[ch - 'a']++; 
+        sort(begin(mp), end(mp), greater<int>());
+        int ans = 0;
+        for(int i = 0; i < 26; i++){
+            ans += mp[i]*((i/8) + 1);
+        }
+        return ans;
+    }
+};
+
+
+
+t.c. = O(nlogN)
