e2b979caacded4625d7fcbf8b0568f6cb40cb5d2
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-08-01 21:19:32 +0530

    Update 486. Predict the Winner.cpp

--- Medium/486. Predict the Winner.cpp
+++ Medium/486. Predict the Winner.cpp
@@ -26,3 +26,30 @@ Constraints:
 
 1 <= nums.length <= 20
 0 <= nums[i] <= 107
+
+
+ //solution
+ class Solution {
+public:
+    int n;
+    int t[23][23];
+    int maxDiff(vector<int>& nums, int l, int r) {
+        if(l == r)
+            return nums[l];
+        if(t[l][r] != -1)
+            return t[l][r];
+        int take_left  = nums[l] - maxDiff(nums, l+1, r);
+        int take_right = nums[r] - maxDiff(nums, l, r-1);
+        
+        return t[l][r] = max(take_left, take_right);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        n = nums.size();
+        memset(t, -1, sizeof(t));
+        return maxDiff(nums, 0, n-1) >= 0;
+            
+    }
+};
+
+
+t.c. = o(n^2)
