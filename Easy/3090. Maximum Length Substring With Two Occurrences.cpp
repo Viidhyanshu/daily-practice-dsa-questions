2069f1404a97fb0086d9269a19f038a34636d505
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-08-14 10:50:32 +0530

    Update 3090. Maximum Length Substring With Two Occurrences.cpp

--- Easy/3090. Maximum Length Substring With Two Occurrences.cpp
+++ Easy/3090. Maximum Length Substring With Two Occurrences.cpp
@@ -25,5 +25,28 @@ Constraints:
 
 2 <= s.length <= 100
 s consists only of lowercase English letters.
- 
+
+
+//solution
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        array<int, 26> count{};
+        int left = 0;
+        int res = 0;
+        for (int right = 0; right < s.length(); ++right) {
+            int ch = s[right] - 'a';
+            ++count[ch];
+            while (count[ch] > 2) {
+                const int ch2 = s[left] - 'a';
+                --count[ch2];
+                ++left;
+            }
+            res = max(res, right - left + 1);
+        }
+        return res;
+    }
+};
+
+t.c. = o(n)
 
