c36d35c366f8a7f64412d0136d5f9600edcb3572
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-08-16 14:06:45 +0530

    Update 2029. Stone Game IX.cpp

--- Medium/2029. Stone Game IX.cpp
+++ Medium/2029. Stone Game IX.cpp
@@ -41,3 +41,24 @@ Constraints:
 
   //solution
   
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0};
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+        int cnt0 = cnt[0];
+        int cnt1 = cnt[1];
+        int cnt2 = cnt[2];
+        if (cnt1 == 0 && cnt2 == 0)
+            return false;
+        if (cnt0 % 2 == 0) {
+            return cnt1 > 0 && cnt2 > 0;
+        }
+        return abs(cnt1 - cnt2) >= 3;
+    }
+};
+
+t.c. = O(N)
+ s.c. = O(1)
