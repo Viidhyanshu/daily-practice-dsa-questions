279d56bffc38c61e5cecd074e9f34fcf56164507
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-09-13 21:57:22 +0530

    Update 835. Image Overlap.cpp

--- Medium/835. Image Overlap.cpp
+++ Medium/835. Image Overlap.cpp
@@ -1,1 +1,64 @@
+You are given two images, img1 and img2, represented as binary, square matrices of size n x n. A binary matrix has only 0s and 1s as values.
 
+We translate one image however we choose by sliding all the 1 bits left, right, up, and/or down any number of units. We then place it on top of the other image. We can then calculate the overlap by counting the number of positions that have a 1 in both images.
+
+Note also that a translation does not include any kind of rotation. Any 1 bits that are translated outside of the matrix borders are erased.
+
+Return the largest possible overlap.
+
+ 
+
+Example 1:
+
+
+Input: img1 = [[1,1,0],[0,1,0],[0,1,0]], img2 = [[0,0,0],[0,1,1],[0,0,1]]
+Output: 3
+Explanation: We translate img1 to right by 1 unit and down by 1 unit.
+
+The number of positions that have a 1 in both images is 3 (shown in red).
+
+Example 2:
+
+Input: img1 = [[1]], img2 = [[1]]
+Output: 1
+Example 3:
+
+Input: img1 = [[0]], img2 = [[0]]
+Output: 0
+ 
+
+Constraints:
+
+n == img1.length == img1[i].length
+n == img2.length == img2[i].length
+1 <= n <= 30
+img1[i][j] is either 0 or 1.
+img2[i][j] is either 0 or 1.
+
+
+  //solution
+  class Solution {
+public:
+    int countOverlaps(vector<vector<int>>& A, vector<vector<int>>& B, int rowOff, int colOff) {
+        int n = A.size();
+        int count = 0;
+        for(int row = 0; row < n; row++) {
+            for(int col = 0; col < n; col++) {
+                if(row+rowOff < 0 || row+rowOff >= n || col+colOff < 0 || col+colOff >= n)
+                    continue;
+                count += A[row][col]*B[row+rowOff][col+colOff];
+            }
+        }
+        return count;
+    }
+    int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+        int n = A.size();
+        int maxOverlap = 0;
+        for(int rowOff = -n+1; rowOff<n; rowOff++) {
+            for(int collOff = -n+1; collOff<n; collOff++) {
+                maxOverlap = max(maxOverlap, countOverlaps(A, B, rowOff, collOff));
+            }
+        }
+        return maxOverlap;
+    }
+};
