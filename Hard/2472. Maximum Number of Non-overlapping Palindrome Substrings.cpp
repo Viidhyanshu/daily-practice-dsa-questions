3034a9373e2e4c0bb336fa51d9579165b81e1601
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-09-16 14:23:12 +0530

    Update 2472. Maximum Number of Non-overlapping Palindrome Substrings.cpp

--- Hard/2472. Maximum Number of Non-overlapping Palindrome Substrings.cpp
+++ Hard/2472. Maximum Number of Non-overlapping Palindrome Substrings.cpp
@@ -27,3 +27,41 @@ Constraints:
 
 1 <= k <= s.length <= 2000
 s consists of lowercase English letters.
+
+
+
+
+ //solution
+ class Solution {
+public:
+    int maxPalindromes(string s, int k) {
+        int n = s.size();
+        int ans = 0, start = 0;
+
+        auto check = [&](int l, int r) {
+            while (l < r) {
+                if (s[l++] != s[r--]) {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        for (int r = k - 1; r < n; ++r) {
+            int l = r - k + 1;
+            if (l >= start && check(l, r)) {
+                ++ans;
+                start = r + 1;
+                continue;
+            }
+
+            l = r - k;
+            if (l >= start && check(l, r)) {
+                ++ans;
+                start = r + 1;
+            }
+        }
+
+        return ans;
+    }
+};
