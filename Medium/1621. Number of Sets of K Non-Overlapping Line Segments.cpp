fc75ee270a91903d1c79de8dddbbf7e46726b566
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-09-16 14:22:10 +0530

    Update 1621. Number of Sets of K Non-Overlapping Line Segments.cpp

--- Medium/1621. Number of Sets of K Non-Overlapping Line Segments.cpp
+++ Medium/1621. Number of Sets of K Non-Overlapping Line Segments.cpp
@@ -27,3 +27,30 @@ Constraints:
 
 2 <= n <= 1000
 1 <= k <= n-1
+
+
+ //solution
+ class Solution {
+public:
+    int M = 1e9 + 7;
+    int dp[1001][1001];
+    int numberOfSets(int n, int K) {
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = (i < n) ? 1 : 0;
+        }
+        for (int k = 1; k <= K; k++) {
+            vector<int> prevRowSum(n + 1, 0);
+            for (int x = n - 1; x >= 0; x--) {
+                prevRowSum[x] = (prevRowSum[x + 1] + dp[k - 1][x]) % M;
+            }
+            for (int i = n - 1; i >= 0; i--) {
+                int skip = dp[k][i + 1];
+                int take = prevRowSum[i+1];
+                dp[k][i] = (take + skip) % M;
+            }
+        }
+        return dp[K][0];
+    }
+};
+
+t.c. = o(n x k)
