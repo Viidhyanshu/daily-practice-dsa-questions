7240a377924b4e8a7e6410a94baaccb402b63909
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-09-21 13:09:27 +0530

    Add resultArray function to compute values based on k
    Implement a solution to find values based on the given array and k.

--- Medium/3524. Find X Value of Array I.cpp
+++ Medium/3524. Find X Value of Array I.cpp
@@ -62,3 +62,27 @@ Constraints:
 1 <= nums[i] <= 109
 1 <= nums.length <= 105
 1 <= k <= 5
+
+//solution
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<long long> result(k);
+        vector<long long> dp(k);
+        for (int i = 0; i < n; i++) {
+            vector<long long> ndp(k);  
+            ndp[nums[i] % k]++;
+            for (int r = 0; r < k; r++) {
+                ndp[(long long)r * nums[i] % k] += dp[r];
+            }
+            dp = move(ndp);
+            for (int r = 0; r < k; r++) {
+                result[r] += dp[r];
+            }
+        }
+        return result;
+    }
+};
+
+t.c. =O(n x k)
