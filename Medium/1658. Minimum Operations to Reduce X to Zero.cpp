ce23cdf197a7a73d4dbedb4d6102139148b121d1
Vidhyanshu Kumar <101850664+Viidhyanshu@users.noreply.github.com>
2026-09-23 17:30:49 +0530

    Update 1658. Minimum Operations to Reduce X to Zero.cpp

--- Medium/1658. Minimum Operations to Reduce X to Zero.cpp
+++ Medium/1658. Minimum Operations to Reduce X to Zero.cpp
@@ -26,3 +26,31 @@ Constraints:
 1 <= nums.length <= 105
 1 <= nums[i] <= 104
 1 <= x <= 109
+
+
+ //solution
+ class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {        
+        int n    = nums.size();
+        int sum  = 0;
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        for(int i = 0; i<n; i++) {
+            sum += nums[i];
+            mp[sum] = i;
+        }
+        if(sum < x)
+            return -1;
+        int restSum  = sum-x;
+        int longest  = INT_MIN;
+        sum = 0;
+        for(int i = 0; i<n; i++) {
+            sum += nums[i];
+            if(mp.count(sum-restSum)) {
+                longest = max(longest, i-mp[sum-restSum]);
+            }
+        }
+        return longest==INT_MIN?-1:n-longest;
+    }
+};
